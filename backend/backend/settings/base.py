5716d1c3dbe736214743eec27d25acafcec3529f
Carmen <carmengalvan@z1.digital>
2023-12-14 09:46:05 +0100

    feat: creation model ReservedSlot

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "media_upload",
     "users",
     "resources",
+    "slots",
     "corsheaders",
     "health_check",
 ]
---
+++ backend/slots/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from slots.models import ReservedSlot
+
+# Register your models here.
+admin.site.register(ReservedSlot)
