22595faaf4aac12aff5a187e87f2b7118bef303d
Carmen <carmengalvan@z1.digital>
2023-12-19 10:20:22 +0100

    feat: registration including public_name and login with token

--- backend/users/graphql/types.py
+++ backend/users/graphql/types.py
@@ -2,8 +2,6 @@ from datetime import datetime
 from uuid import UUID
 
 import strawberry
-from strawberry.types import Info
-from strawberry_django_jwt.settings import jwt_settings
 
 
 @strawberry.type
@@ -12,16 +10,12 @@ class UserType:
     first_name: str
     last_name: str
     email: str
+    public_name: str
     created: datetime
 
-    @strawberry.field
-    def token(self, info: Info) -> str:
-        if hasattr(self, "token"):
-            return self.token
-        auth = info.context.request.META.get(
-            jwt_settings.JWT_AUTH_HEADER_NAME, ""
-        ).split()
-        prefix = jwt_settings.JWT_AUTH_HEADER_PREFIX
-        if len(auth) == 2 and auth[0].lower() == prefix.lower():
-            return auth[1]
-        return ""
+
+@strawberry.type
+class UserTypeWeb:
+    user: UserType
+    token: str
+    refresh_token: str
