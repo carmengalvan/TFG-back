444653d27e3ba5d0ae46c025ec2124d8c7b352a2
Carmen <carmengalvan@z1.digital>
2023-12-21 11:09:23 +0100

    test: tests corrected according to the change in registration and login

--- backend/users/graphql/types.py
+++ backend/users/graphql/types.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from uuid import UUID
 
 import strawberry
+from strawberry.types import Info
+from strawberry_django_jwt.settings import jwt_settings
 
 
 @strawberry.type
@@ -12,6 +14,18 @@ class UserType:
     email: str
     public_name: str
     created: datetime
+
+    @strawberry.field
+    def token(self, info: Info) -> str:
+        if hasattr(self, "token"):
+            return self.token
+        auth = info.context.request.META.get(
+            jwt_settings.JWT_AUTH_HEADER_NAME, ""
+        ).split()
+        prefix = jwt_settings.JWT_AUTH_HEADER_PREFIX
+        if len(auth) == 2 and auth[0].lower() == prefix.lower():
+            return auth[1]
+        return ""
 
 
 @strawberry.type
