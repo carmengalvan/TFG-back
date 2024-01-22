11798ed8cbf46b3e97bd82d51f179cf08efdca1c
Carmen <carmengalvan@z1.digital>
2024-01-22 11:16:56 +0100

    test: my resources query positive test

---
+++ backend/resources/tests/requests/queries.py
@@ -0,0 +1,21 @@
+RESOURCES_ITEMS = """
+    query($pagination: PaginationInput!){
+        myResources(pagination: $pagination){
+            pageInfo{
+                page
+                pages
+                totalResults
+            }
+            edges{
+                id
+                name
+                description
+                availableTime
+                startDate
+                endDate
+                location
+            }
+        }
+    }
+
+"""
